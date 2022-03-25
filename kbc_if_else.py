4df8b77dab1dbdf45ec7f1073bd7af0fd74e6be6
Abhimanyu Singh <93311022+AbhimanyuS18@users.noreply.github.com>
2022-03-25 20:14:11 +0530

    kbc

---
+++ kbc_if_else.py
@@ -0,0 +1,137 @@
+######### KBC QUIZ PROJECT ########
+
+
+
+# point = 5
+# a  = int(input("enter choose your lucky number to get Access"))
+# if (a>5 and a<=10):
+#     print("hello")
+#     name = input("enter your name contain 8 letter")
+#     if len(name)>=8 and len(name)<15:
+#         print("welcome our page!!!!")
+#         # print(name)
+#         input("Go the next ! please enter")
+#         # input("Can I ask you any question for test your knowledge")
+#         userinput = input(" Can I ask you any question for test your knowledge  please! choose Yes / No")
+#         if userinput  == "yes":
+#             print("answer this question") 
+#             print("the CEO of microsoft company")
+#             a = print("1 = satya nadela")
+#             b = print("2 = Bill gates")
+#             c = print("3 = Mark jakerberg")
+#             d = print("4 = Elon musk")
+#             e = print("5 = None of these")
+#             # print(a,b,c,d,e)
+#             cloice=input('choose your answer')
+#             if cloice== '1':
+#                     print("""conrates! you get 5 point
+
+# congrates! your answer is right""")
+#                     # print("")
+#                     # print("congrates! your answer is right")
+#                     para = input("Go to next question please! Enter")
+#                     print("the CEO of spaceX company")
+#                     print("1 = sunder pichai")
+#                     print("2 = rakesh sharma")
+#                     print("3 = stephan hacking") 
+#                     print("4 = Elon musk")
+#                     answer = input("choose your answer")
+#                     if answer == "4":
+#                         print("congrates! you are right")
+#                         input("Go to the next question please ! Enter")
+#                         print("The full name of ISRO")
+#                         print("1 = japan aerospace exploration agency")
+#                         print("2 = indian space research orgination")
+#                         print("3 = canadian space agency")
+#                         print("4 = national aeronutical space agency ")
+#                         ans = input("choose the answer")
+#                         if ans == "2":
+#                             print("congrates! you are pass in the test")
+#                         else:
+#                             print("sorry! your answer is wrong")
+#                             print("     So you are fail in test")
+#                     else:
+#                         print("your answer is wrong! pls try later")        
+#             else:
+#                 print("your answer is wrong! pls try later")   
+#         else:
+#             print("please! choose yes / no ")
+#     else:
+#         print("please! enter the name between 8 to 15 character")
+# else:
+#     print("please! choose the right lucky number to Access this page")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+point = 5
+a  = int(input("enter choose your lucky number to get Access"))
+if (a>5 and a<=10):
+    print("hello")
+    name = input("enter your name contain 8 letter")
+    if len(name)>=8 and len(name)<15:
+        print("welcome our page!!!!")
+        # print(name)
+        input("Go the next ! please enter")
+        # input("Can I ask you any question for test your knowledge")
+        userinput = input(" Can I ask you any question for test your knowledge  please! choose Yes / No")
+        if userinput  == "yes":
+            print("answer this question") 
+            print("the CEO of microsoft company")
+            a = print("1 = satya nadela")
+            b = print("2 = Bill gates")
+            c = print("3 = Mark jakerberg")
+            d = print("4 = Elon musk")
+            e = print("5 = None of these")
+            # print(a,b,c,d,e)
+            cloice=input('choose your answer')
+            if cloice== '1':
+                        print("""                   congrates! your answer is right
+                    so you are get the 5 point """)
+                    # print("")
+                    # print("congrates! your answer is right")
+                        input("Go to next question please! Enter")
+                        print("the CEO of spaceX company")
+                        print("1 = sunder pichai")
+                        print("2 = rakesh sharma")
+                        print("3 = stephan hacking") 
+                        print("4 = Elon musk")
+                        answer = input("choose your answer")
+                        if answer == "4":
+                            print("""congrates! your answer is right
+                             you get 10 point""")
+                            input("Go to the next question please ! Enter")
+                            print("The full name of ISRO")
+                            print("1 = japan aerospace exploration agency")
+                            print("2 = indian space research orgination")
+                            print("3 = canadian space agency")
+                            print("4 = national aeronutical space agency ")
+                            ans = input("choose the answer")
+                            if ans == "2":
+                                print("""congrates! your answer is right so you are pass in this test""")
+                                print("you get total 15 score in this test",)
+                                # print("congrates! you are pass in the test")
+                            else:
+                                print("sorry! your answer is wrong")
+                                print("     So you are fail in test")
+                        else:
+                            print("your answer is wrong! pls try later\n and you get the 15 point in this test")  
+                      
+            else:
+                print("your answer is wrong! pls try later")   
+        else:
+            print("please! choose yes / no ")
+    else:
+        print("please! enter the name between 8 to 15 character")
+else:
+    print("please! choose the right lucky number to Access this page")
